0d3861b2d4ac6131797643f04dfa0bb345475e8d
Mike Huang <wellwind@mail2000.com.tw>
2022-01-27 14:28:15 +0800

    補充 RxJS 用法

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -2,27 +2,50 @@ import { Article } from './../../interfaces/article';
 import { PostService } from './../../post.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
-
   article?: Article;
 
-  constructor(private postService: PostService, private route: ActivatedRoute) { }
+  article$?: Observable<Article>;
+
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(paramMap => {
-      console.log(paramMap.get('id'));
-      const id = paramMap.get('id')
-      this.postService.getArticle(id).subscribe(result => {
-        console.log(result);
-        this.article = result.article;
-      })
-    })
-  }
+    // this.route.paramMap.subscribe(paramMap => {
+    //   console.log(paramMap.get('id'));
+    //   const id = paramMap.get('id')
+    //   this.postService.getArticle(id).subscribe(result => {
+    //     console.log(result);
+    //     this.article = result.article;
+    //   })
+    // })
 
+    // this.route.paramMap
+    //   .pipe(
+    //     map((paramMap) => paramMap.get('id')),
+    //     switchMap((id) => this.postService.getArticle(id)),
+    //     map(result => result.article)
+    //   )
+    //   .subscribe((result) => {
+    //     // console.log(result);
+    //     this.article = result;
+    //   });
+
+    this.article$ = this.route.paramMap
+      .pipe(
+        map((paramMap) => paramMap.get('id')),
+        switchMap((id) => this.postService.getArticle(id)),
+        map(result => result.article)
+      );
+  }
 }
