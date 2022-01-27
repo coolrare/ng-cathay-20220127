e514a28ad0e347f9d8c5515e37494289daaeb282
Mike Huang <wellwind@mail2000.com.tw>
2022-01-27 14:37:05 +0800

    任務 04：在程式中動態切換路由

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,41 @@
+import { LoginService } from './../login.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginInfo = {
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    // this.loginService.login(this.loginInfo).subscribe(result => {
+    //   console.log(result);
+    //   localStorage.setItem('token', result.user.token);
+    //   this.router.navigateByUrl('/');
+    // });
+
+    this.loginService.login(this.loginInfo).subscribe({
+      next: (result) => {
+        console.log(result);
+        localStorage.setItem('token', result.user.token);
+        this.router.navigateByUrl('/');
+      },
+      error: (error: HttpErrorResponse) => {
+        if(error.status === 401) {
+          alert('登入失敗')
+        }
+      },
+    });
   }
-
 }
